c04270aabb94769926efc35968bb2102682b39fa
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-07-01 16:05:03 -0400

    [ie-webpack-fix] removing package w/o pollyfil in IE

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axe from 'react-axe';
 import AppContainer from './containers/AppContainer';
 
 require("core-js");
 require('../css/main.scss');
 
 const documentLocation = document.getElementById('app');
-
-if (process.env.NODE_ENV !== 'production') {
-    // logs accessibility issues to the dev console
-    axe(React, ReactDOM, 10000);
-}
 
 ReactDOM.render(
     <AppContainer />,
