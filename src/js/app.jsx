95e32f0dd277feb87632a9e157ea5ebf0d4c592c
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-07-01 15:13:19 -0400

    [webpack_ie_troubleshooting] adding react-axe

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axe from 'react-axe';
 import AppContainer from './containers/AppContainer';
 
 require("core-js");
@@ -7,8 +8,12 @@ require('../css/main.scss');
 
 const documentLocation = document.getElementById('app');
 
+if (process.env.NODE_ENV !== 'production') {
+    // logs accessibility issues to the dev console
+    axe(React, ReactDOM, 10000);
+}
+
 ReactDOM.render(
     <AppContainer />,
     documentLocation
 );
-
